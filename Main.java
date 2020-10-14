612b7f4f1b7430e0c684c960b167bada74f3887c
nurtt0949 <nurtt0949@wrdsb.ca>
2020-10-14 14:46:48 +0000

    Final Product

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
  * @author Tolga Nurtekin
  */
 public class Main {
-     
+    //create a method that counts the amount of ones in a number
     public static int countsOnes(int number){
        //create a variable that counts the digits
        int count = 0;
@@ -13,7 +13,7 @@ public class Main {
         int digit = number % 10;
         //divide the number by 10 to move onto next digit
         number /= 10;
-        
+        //if the digit is 1 in the number then add 1 to count
         if(digit == 1){
         count++;
         }
@@ -29,7 +29,7 @@ public class Main {
    */
   public static void main(String[] args) {
     //create a variable for the total amount of ones
-    int total = countsOnes(357);
+    int total = countsOnes(3571111);
     //tell user the amount of ones they have in their number
     System.out.println(total);
   } 
